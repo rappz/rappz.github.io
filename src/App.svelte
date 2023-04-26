50773bbe14693743589a0484742171bec5038074
Zachary Rapp <zachary.rapp@outlook.com>
2023-04-26 09:51:37 -0700

    added login box

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
-	import Nav from "./lib/nav.svelte";
+  import Nav from "./lib/nav.svelte";
   import Login from "./lib/Login.svelte";
 </script>
-<div class="fixed align-middle bg-gray-900 text-white shadow-lg">
-<Login />
+
+<div class="bg-gray-700">
+  <div class="flex items-center justify-center h-screen">
+    <div class="bg-gray-900 text-white shadow-lg w-72 h-72 login-box">
+      <h3 class="flex items-center justify-center m-6 mb-0 text-green-500">
+        FRCS Chat
+      </h3>
+      <Login />
+    </div>
+  </div>
+  <div class="flex">
+    <div
+      class="fixed top-0 left-0 h-screen w-16 m-0
+            flex flex-col
+            bg-gray-900 text-white shadow-lg"
+    >
+      <Nav width="20" height="20" icon="fa6-solid:fire" tooltip="tooltip" />
+      <Nav width="32" height="32" icon="bi:plus" tooltip="tooltip" />
+      <Nav width="20" height="20" icon="bi:lightning-fill" tooltip="tooltip" />
+      <Nav width="20" height="20" icon="fa6-solid:poo" tooltip="tooltip" />
+    </div>
+  </div>
 </div>
 
-<div class="flex">
-    <div class="fixed top-0 left-0 h-screen w-16 m-0
-            flex flex-col
-            bg-gray-900 text-white shadow-lg">
-    <Nav width="20" height="20" icon="fa6-solid:fire" tooltip="tooltip"/>
-    <Nav width="32" height="32" icon="bi:plus" tooltip="tooltip"/>
-    <Nav width="20" height="20" icon="bi:lightning-fill" tooltip="tooltip"/>
-    <Nav width="20" height="20" icon="fa6-solid:poo" tooltip="tooltip"/>
-    </div>
-</div>
-<div class="fixed align-middle bg-gray-900 text-white shadow-lg">
-<Login />
-</div>
 <style lang="postcss">
-   
 </style>
--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,51 +1,58 @@
-<script lang='ts'>
-    import { currentUser, pb } from "./pokectbase";
+<script lang="ts">
+  import { currentUser, pb } from "./pokectbase";
 
-    let username:string;
-    let password:string;
+  let username: string;
+  let password: string;
 
-    async function login() {
-        await pb.collection('users').authWithPassword(username,password);
+  async function login() {
+    await pb.collection("users").authWithPassword(username, password);
+  }
+
+  async function signUp() {
+    try {
+      const data = {
+        username,
+        password,
+        passwordConfirm: password,
+        name: "hi mom!",
+      };
+      const createdUser = await pb.collection("users").create(data);
+      await login();
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    async function signUp() {
-        try{
-            const data = {
-                username,
-                password,
-                passwordConfirm: password,
-                name: 'hi mom!',
-
-            };
-            const createdUser = await pb.collection('users').create(data);
-            await login();
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    function signOut() {
-        pb.authStore.clear();
-    }
-
+  function signOut() {
+    pb.authStore.clear();
+  }
 </script>
 
-{#if $currentUser}
-    <p>Signed in as {$currentUser.username}</p>
-{:else}
+<div class="flex items-center justify-center h-full w-full mt-0">
+  {#if $currentUser}
+    <div class="flex flex-col">
+      <p class="text-green-500">Signed in as {$currentUser.username}</p>
+      <button class="login-button flex" on:click={signOut}>Logout</button>
+    </div>
+  {:else}
     <form on:submit|preventDefault>
-        <input
-            placeholder="Username"
-            type="text"
-            bind:value={username}
-        />
+      <input
+        placeholder="Username"
+        type="text"
+        bind:value={username}
+        class="login-text flex justify-center"
+      />
 
-        <input
-            placeholder="Password"
-            type="password"
-            bind:value={password}
-        />
-        <button on:click={signUp}>Sign Up</button>
-        <button on:click={login}>Login</button>
+      <input
+        placeholder="Password"
+        type="password"
+        bind:value={password}
+        class="login-text flex justify-center"
+      />
+      <div class="flex">
+        <button class="login-button" on:click={signUp}>Sign Up</button>
+        <button class="login-button" on:click={login}>Login</button>
+      </div>
     </form>
-{/if}
+  {/if}
+</div>
